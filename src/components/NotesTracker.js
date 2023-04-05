f55a94b51bd036c76906520f63f8165757654ef1
Fortune Ndlovu <109510679+Fortune-Ndlovu@users.noreply.github.com>
2023-04-05 19:30:18 +0100

    Component Triggering Fixed
    When the user was adding their note the NoteTracker component was triggering the form submission in the parent component EditCollegeModule. To fix this we just added a function in our NotesTracker component to handle the adding of a note using e.preventDefault(); preventing the default form submission

--- src/components/NotesTracker.js
+++ src/components/NotesTracker.js
@@ -2,7 +2,12 @@ import React from "react";
 
 // Created a separate Notes component for the notes functionality
 const NotesTracker = ({ notes, onAddNote, onNoteChange, onDeleteNote }) => {
-  return (
+    let handleAddNote = (e) => {
+        e.preventDefault();
+        onAddNote();
+    }
+
+    return (
     <div>
       <h4>Notes</h4>
       {/* for every note in notes we want to be able to modify within, delete it and add a new note to do the same*/}
@@ -15,7 +20,7 @@ const NotesTracker = ({ notes, onAddNote, onNoteChange, onDeleteNote }) => {
           <button onClick={() => onDeleteNote(index)}>Delete</button>
         </div>
       ))}
-      <button onClick={onAddNote}>Add Note</button>
+      <button onClick={handleAddNote}>Add Note</button>
     </div>
   );
 };
