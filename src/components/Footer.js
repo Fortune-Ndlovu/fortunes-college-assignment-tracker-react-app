59e20f38a27c35d64500ab029e6488cf347e8409
Fortune Ndlovu <109510679+Fortune-Ndlovu@users.noreply.github.com>
2023-04-11 15:20:09 +0100

    centering footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,65 +1,36 @@
 import React from 'react';
-import { CDBLink, CDBBox, CDBBtn, CDBIcon } from 'cdbreact';
+import { CDBBox, CDBBtn, CDBIcon } from 'cdbreact';
 
 const Footer = () => {
   return (
-    <CDBBox  className="shadow">
-      <CDBBox display="flex" flex="column" className="mx-auto py-5" style={{ width: '90%' }}>
-        <CDBBox display="flex" justifyContent="between" className="flex-wrap">
-          <CDBBox>
-            <a href="/" className="d-flex align-items-center p-0 text-dark">
-              <span className="ms-3 h5 font-weight-bold">Module Tracker</span>
-            </a>
-            <p className="my-3" style={{ width: '250px' }}>
-              We are helping folks manage thier college modules more efficiently.
-            </p>
-            <CDBBox display="flex" className="mt-4">
-              <CDBBtn flat color="dark">
-                <CDBIcon fab icon="facebook-f" />
-              </CDBBtn>
-              <CDBBtn flat color="dark" className="mx-3">
-                <CDBIcon fab icon="twitter" />
-              </CDBBtn>
-              <CDBBtn flat color="dark" className="p-2">
-                <CDBIcon fab icon="instagram" />
-              </CDBBtn>
+    <footer>
+      <CDBBox  className="footer">
+        <CDBBox display="flex" flex="column" className="mx-auto py-5" style={{ width: '90%' }}>
+          <CDBBox display="flex" justifyContent="between" className="flex-wrap">
+            <CDBBox>
+              <a href="/" className="d-flex align-items-center text-dark">
+                <span className="h5 font-weight-bold">Module Tracker</span>
+              </a>
+              <p className="my-3" style={{ width: '250px' }}>
+                We are helping folks manage thier college modules more efficiently.
+              </p>
+              <CDBBox display="flex" className="mt-4" id="footer-icons">
+                <CDBBtn flat color="dark">
+                  <CDBIcon fab icon="facebook-f" />
+                </CDBBtn>
+                <CDBBtn flat color="dark" className="mx-3">
+                  <CDBIcon fab icon="twitter" />
+                </CDBBtn>
+                <CDBBtn flat color="dark" className="p-2">
+                  <CDBIcon fab icon="instagram" />
+                </CDBBtn>
+              </CDBBox>
             </CDBBox>
-          </CDBBox>
-          <CDBBox>
-            <p className="h5 mb-4" style={{ fontWeight: '600' }}>
-            Module Tracker
-            </p>
-            <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
-              <CDBLink href="/">Resources</CDBLink>
-              <CDBLink href="/">About Us</CDBLink>
-              <CDBLink href="/">Contact</CDBLink>
-              <CDBLink href="/">Blog</CDBLink>
             </CDBBox>
-          </CDBBox>
-          <CDBBox>
-            <p className="h5 mb-4" style={{ fontWeight: '600' }}>
-              Help
-            </p>
-            <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
-              <CDBLink href="/">Support</CDBLink>
-              <CDBLink href="/">Sign Up</CDBLink>
-              <CDBLink href="/">Sign In</CDBLink>
-            </CDBBox>
-          </CDBBox>
-          <CDBBox>
-            <p className="h5 mb-4" style={{ fontWeight: '600' }}>
-              Products
-            </p>
-            <CDBBox flex="column" style={{ cursor: 'pointer', padding: '0' }}>
-              <CDBLink href="/">Windframe</CDBLink>
-              <CDBLink href="/">Loop</CDBLink>
-              <CDBLink href="/">Contrast</CDBLink>
-            </CDBBox>
-          </CDBBox>
+          <small className="text-center mt-5">&copy; Module Tracker, 2023. All rights reserved.</small>
         </CDBBox>
-        <small className="text-center mt-5">&copy; Module Tracker, 2023. All rights reserved.</small>
-      </CDBBox>
-    </CDBBox >
+      </CDBBox >
+    </footer>
   );
 };
 
